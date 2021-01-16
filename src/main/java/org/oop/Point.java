ebcb0a074ee8c7e9bab9d2c052f1735d17accfcd
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-01-16 22:03:53 +0530

    Refactor: Extract method for distance calculation

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -9,16 +9,19 @@ public class Point {
         this.y = y;
     }
 
+    private static int getDifference(int coordinate1, int coordinate2) {
+        return coordinate1 - coordinate2;
+    }
+
     public static double distanceBetween(Point from, Point to) {
-        double xDistance = to.x - from.x;
-        double yDistance = to.y - from.y;
+        double xDistance = getDifference(to.x, from.x);
+        double yDistance = getDifference(to.y, from.y);
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public static double directionOf(Point from, Point to) {
-        double xDistance = to.x - from.x;
-        double yDistance = to.y - from.y;
+        double xDistance = getDifference(to.x, from.x);
+        double yDistance = getDifference(to.y, from.y);
         return Math.atan2(yDistance, xDistance);
     }
-
 }
